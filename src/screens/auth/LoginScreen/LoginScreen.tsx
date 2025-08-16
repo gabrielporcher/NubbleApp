90bee128ed7ada3fad38d491a78ef2fc0282b78e
gabrielporcher <gabrielporcher95@gmail.com>
2025-08-16 19:13:44 -0300

    tratamento de formularios

--- src/screens/auth/LoginScreen/LoginScreen.tsx
+++ src/screens/auth/LoginScreen/LoginScreen.tsx
@@ -1,21 +1,39 @@
 import React from 'react';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../../routes/Routes';
 import {Text} from '../../../components/Text';
 import {Button} from '../../../components/Button/Button';
 import {TextInput} from '../../../components/TextInput/TextInput';
 import {PasswordInput} from '../../../components/PasswordInput/PasswordInput';
 import {Screen} from '../../../components/Screen/Screen';
+import {useForm, Controller} from 'react-hook-form';
+import {Alert} from 'react-native';
 
+type LoginFormType = {
+  email: string;
+  password: string;
+};
 type ScreenProps = NativeStackScreenProps<RootStackParamList, 'LoginScreen'>;
 
 export function LoginScreen({navigation}: ScreenProps) {
+  const {control, formState, handleSubmit} = useForm<LoginFormType>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'onChange'
+  });
+
   function navigateToSignupScreen() {
     navigation.navigate('SignUpScreen');
   }
 
   function navigateToForgotPasswordScreen() {
     navigation.navigate('ForgotPasswordScreen');
+  }
+
+  function submitForm({email, password}: LoginFormType) {
+    Alert.alert(`Email: ${email} ${`\n`} Senha: ${password}`);
   }
 
   return (
@@ -27,25 +45,70 @@ export function LoginScreen({navigation}: ScreenProps) {
         Digite seu e-mail e senha para entrar
       </Text>
 
-      <TextInput
-        errorMessage="mensagem de error"
-        label="E-mail"
-        placeholder="Digite seu e-mail"
-        boxProps={{mb: 's20'}}
+      <Controller
+        control={control}
+        name="email"
+        rules={{
+          required: 'Email obrigatório',
+          pattern: {
+            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+            message: 'E-mail inválido',
+          },
+        }}
+        render={({field, fieldState}) => (
+          <TextInput
+            value={field.value}
+            onChangeText={field.onChange}
+            errorMessage={fieldState.error?.message}
+            label="E-mail"
+            placeholder="Digite seu e-mail"
+            boxProps={{mb: 's20'}}
+          />
+        )}
       />
 
-      <PasswordInput
-        label="Senha"
-        placeholder="Digite sua senha"
-        boxProps={{mb: 's10'}}
+      <Controller
+        control={control}
+        name="password"
+        rules={{
+          required: 'Senha obrigatória',
+          minLength: {
+            value: 6,
+            message: 'A senha deve ter pelo menos 6 caracteres'
+          }
+        }}
+        render={({field, fieldState}) => (
+          <PasswordInput
+            value={field.value}
+            onChangeText={field.onChange}
+            errorMessage={fieldState.error?.message}
+            label="Senha"
+            placeholder="Digite sua senha"
+            boxProps={{mb: 's20'}}
+          />
+        )}
       />
 
-      <Text onPress={navigateToForgotPasswordScreen} color="primary" preset="paragraphSmall" bold>
+      <Text
+        onPress={navigateToForgotPasswordScreen}
+        color="primary"
+        preset="paragraphSmall"
+        bold>
         Esqueci minha senha
       </Text>
 
-      <Button marginTop="s48" title="Entrar" />
-      <Button onPress={navigateToSignupScreen} preset="outline" marginTop="s12" title="Criar uma conta" />
+      <Button
+        marginTop="s48"
+        title="Entrar"
+        onPress={handleSubmit(submitForm)}
+        disabled={!formState.isValid}
+      />
+      <Button
+        onPress={navigateToSignupScreen}
+        preset="outline"
+        marginTop="s12"
+        title="Criar uma conta"
+      />
     </Screen>
   );
 }
--- src/screens/auth/SignUpScreen/SignUpScreen.tsx
+++ src/screens/auth/SignUpScreen/SignUpScreen.tsx
@@ -8,13 +8,31 @@ import {Icon} from '../../../components/Icon/Icon';
 import {Button} from '../../../components/Button/Button';
 import {PasswordInput} from '../../../components/PasswordInput/PasswordInput';
 import {useResetNavigationSuccess} from '../../../hooks/useResetNavigationSuccess';
+import {useForm, Controller} from 'react-hook-form';
 
+type SignupFormType = {
+  username: string;
+  fullName: string;
+  email: string;
+  password: string;
+};
 type ScreenProps = NativeStackScreenProps<RootStackParamList, 'SignUpScreen'>;
 
 export function SignUpScreen({navigation}: ScreenProps) {
   const {reset} = useResetNavigationSuccess();
+  const {control, formState, handleSubmit} = useForm<SignupFormType>({
+    defaultValues: {
+      username: '',
+      fullName: '',
+      email: '',
+      password: '',
+    },
+    mode: 'onChange',
+  });
 
-  function submitForm() {
+  function submitForm(formValues: SignupFormType) {
+    console.log(formValues);
+    /*
     reset({
       title: 'Sua conta foi criada com sucesso',
       description: 'Agora você é só fazer login na nossa plataforma',
@@ -23,6 +41,7 @@ export function SignUpScreen({navigation}: ScreenProps) {
         color: 'success',
       },
     });
+    */
   }
 
   return (
@@ -30,28 +49,93 @@ export function SignUpScreen({navigation}: ScreenProps) {
       <Text preset="headingLarge" marginBottom="s32">
         Criar uma conta
       </Text>
-      <TextInput
-        label="Seu username"
-        placeholder="@"
-        boxProps={{marginBottom: 's20'}}
+
+      <Controller
+        control={control}
+        name="username"
+        rules={{
+          required: 'Username obrigatório',
+        }}
+        render={({field, fieldState}) => (
+          <TextInput
+            value={field.value}
+            onChangeText={field.onChange}
+            errorMessage={fieldState.error?.message}
+            label="Seu username"
+            placeholder="@"
+            boxProps={{marginBottom: 's20'}}
+          />
+        )}
       />
-      <TextInput
-        label="Nome completo"
-        placeholder="Digite seu nome completo"
-        boxProps={{marginBottom: 's20'}}
+
+      <Controller
+        control={control}
+        name="fullName"
+        rules={{
+          required: 'Nome completo obrigatório',
+        }}
+        render={({field, fieldState}) => (
+          <TextInput
+            value={field.value}
+            onChangeText={field.onChange}
+            errorMessage={fieldState.error?.message}
+            label="Nome completo"
+            placeholder="Digite seu nome completo"
+            boxProps={{marginBottom: 's20'}}
+          />
+        )}
       />
-      <TextInput
-        label="E-mail"
-        placeholder="Digite seu email"
-        boxProps={{marginBottom: 's20'}}
+
+      <Controller
+        control={control}
+        name="email"
+        rules={{
+          required: 'Email obrigatório',
+          pattern: {
+            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+            message: 'E-mail inválido',
+          },
+        }}
+        render={({field, fieldState}) => (
+          <TextInput
+            value={field.value}
+            onChangeText={field.onChange}
+            errorMessage={fieldState.error?.message}
+            label="E-mail"
+            placeholder="Digite seu email"
+            boxProps={{marginBottom: 's20'}}
+          />
+        )}
       />
-      <PasswordInput
-        label="Senha"
-        secureTextEntry
-        placeholder="Digite sua senha"
-        boxProps={{mb: 's48'}}
+
+      <Controller
+        control={control}
+        name="password"
+        rules={{
+          required: 'Senha obrigatória',
+          minLength: {
+            value: 6,
+            message: 'A senha deve ter pelo menos 6 caracteres',
+          },
+        }}
+        render={({field, fieldState}) => (
+          <PasswordInput
+            value={field.value}
+            onChangeText={field.onChange}
+            errorMessage={fieldState.error?.message}
+            label="Senha"
+            secureTextEntry
+            placeholder="Digite sua senha"
+            boxProps={{mb: 's48'}}
+          />
+        )}
       />
-      <Button title="Criar uma conta" onPress={submitForm} />
+
+      <Button
+        disabled={!formState.isValid}
+        title="Criar uma conta"
+        onPress={handleSubmit(submitForm)}
+      />
     </Screen>
   );
 }
